d23d63a37deaae53644673b33c59ad7756221ab3
abhi-0086 <babaabhishek86@gmail.com>
2023-07-21 22:03:48 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import Home from "./pages/Home";
+import CountryDetail from "./pages/CountryDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/countries/:countryCode" element={<CountryDetail />} />
+          <Route
+            path="*"
+            element={
+              <h2
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                404 | Page not found
+              </h2>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
